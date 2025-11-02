8614c2d5317c4ae2b8e99452656a980612ff9736
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-11-02 12:06:25 +0300

    Creating the VendingMachine Class also included stocks and amount

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
+class VendingMachine():
+
+    def __init__(self, amount, stocks):
+        self.amount = amount
+        self.stocks = stocks
+
+    def amounts(self):
+       return self.amount
+    def stocks(self):
+        if self.stocks <= 0:
+            print("Out of stock")
+            return False
+        
+        else:
+            self.stocks -= 1
+            print("Transaction was successful")
+            return True
+        
+                
+class chips:
+    count = 50
+
+    def __init__(self, price, chip_types, stock ):
+        self.price = price
+        self.chip_types = chip_types
+        self.stock = stock
+        chips.count -= 1
+    def get_price(self):
+        return f"{self.price}{self.stock}" # INSTANCE 
+    
+    @classmethod
+    def get_count(cls):
+        return f"Total # of Stocks in Chips {cls.count}"
+    
+    
+
+
+    
+    
+    
+    def NameOfChips(self,type):
+         
+
+                   
+
+        
+
+
+
+class drinks:
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+class candy:
 
 
 
@@ -23,4 +63,18 @@
 
 
 # Vending Machine Project
+ 
 print("Welcome to the vending machine!")
+payment_type = input("Coin or Cash")
+try:
+    choice = input("Insert a {payment_type}: \n 1. Chips \n  Drinks \n Candy")
+    if choice == 1:
+        chips()
+
+    elif choice == 2:
+        drinks()
+
+    elif choice == 3:
+        candy()
+except:
+    ValueError("Please enter a number")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,40 @@
+
+#     #def NameOfChips(self,type):
+         
+
+                   
+
+        
+
+
+
+# class drinks:
+
+
+
+
+# class candy:
+
+
+
+
+
+
+
+
+
+# # Vending Machine Project
+ 
+# print("Welcome to the vending machine!")
+# try:
+#     choice = print("Insert a coin: \n 1. Chips \n  Drinks \n Candy")
+#     if choice == 1:
+#         chips()
+
+#     elif choice == 2:
+#         drinks()
+
+#     elif choice == 3:
+#         candy()
+# except:
+#     ValueError("Please enter a number")
